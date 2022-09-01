1101da05c7f30d845242615b54702870ca15f8cb
sahion <alexeysuvorov33@gmail.com>
2022-09-01 17:10:13 +0700

    feat: create get 1 apply

--- src/controllers/applyController.ts
+++ src/controllers/applyController.ts
@@ -2,16 +2,22 @@ import {Request, Response} from 'express';
 import { IApply } from '../interfaces/IApply';
 import jwt from 'jsonwebtoken';
 import { IUser } from '../interfaces/IUser';
+import fsPromises from 'fs/promises';
+import path from 'path';
 
 const ApplyDB = {
   applies: require('../../data/applies.json') as IApply[],
   setApplies: function (data: IApply[]) {this.applies = data}
 }
 
-const fsPromises  = require('fs').promises;
-const path = require('path');
 
 const getApplies = (req: Request,res: Response) =>{
+  return res.json(ApplyDB.applies);
+}
+
+const getApply = (req: Request,res: Response) =>{
+  const apply = ApplyDB.applies.find(apply => apply.id === +req.params.id);
+  if (!apply) return res.status(404).json({'message': 'такой заявки не существует'});
   return res.json(ApplyDB.applies);
 }
 
@@ -30,4 +36,4 @@ const createApply = async (req: Request,res: Response) => {
   return res.status(200).json({'message': 'Заявка успешно создана'});
   }
 
-module.exports = { getApplies, createApply } ;
+module.exports = { getApplies, createApply, getApply } ;
--- src/routes/apply.ts
+++ src/routes/apply.ts
@@ -7,6 +7,7 @@ const applyController = require('../controllers/applyController');
 const app = require('express')();
 
 router.get('/', applyController.getApplies);
+router.get('/:id', applyController.getApply);
 app.use(verifyJWT);
 router.post('/', applyController.createApply);
 
